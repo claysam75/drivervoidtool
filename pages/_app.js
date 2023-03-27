8f7f62c953ac8aad21ed3dab3f4c28a83296ebd9
Sam Clay <clay.sam75@gmail.com>
2023-03-27 11:45:25 +0100

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
